dc0f7bcd8af2588ed7c6943644d5ddba950b25bb
Meron Tesfamariam <Meron.DTK.Tesfamariam@lystkom.se>
2018-11-09 11:15:58 +0100

    complet fun ans start on  array.s

---
+++ projects/elve-javascript 101/assets/js/5-array.js
@@ -0,0 +1,10 @@
+//list a group of friends
+
+var friend1 = "Måns";
+var friend2 = "Sten";
+var friend3 = "Clases";
+var friend4 = "Måna";
+
+// list a group of friends using ac array
+
+var friends = ["måns", "Sten", "Claes", "Måna"] //string, numbers, booleans, objects
