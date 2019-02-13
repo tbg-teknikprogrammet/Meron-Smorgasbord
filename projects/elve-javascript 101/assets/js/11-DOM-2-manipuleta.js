0f400717447e97e25b5c7185d3ebaa399af05943
Meron Tesfamariam <merontesfamariam@hotmail.com>
2019-02-13 21:59:35 +0100

    events

--- projects/elve-javascript 101/assets/js/11-DOM-2-manipuleta.js
+++ projects/elve-javascript 101/assets/js/11-DOM-2-manipuleta.js
@@ -14,7 +14,18 @@ var ele = document.querySelector("h1");
 ele.classList.add("some-class");
 
 
-var p2 = document.getElementsByClassName("special")[1];
-p2.classList.add("another-class");
+// var p2 = document.getElementsByClassName("special2")[1];
+// p2.classList.add("another-class");
 // firstP.classList.remove("another-class");
 // firstP.classList.toggle("another-class");
+
+
+
+//select the element
+
+var pEle = document.querySelector(".test");
+pEle.textContent = "This is a really <strong></strong> awesome paraggraph";
+pEle.innerHTML = "This is a really <strong></strong> awesome paraggraph";
+
+document.getElementsByTagName("h1")[1].textContent = "The End";
+document.getElementsByTagName("h1")[1].innerHTML = "The <i>End!</i>";
